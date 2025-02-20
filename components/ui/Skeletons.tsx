43d7b24e4b47e36b74be43098e7669404f27d1bb
diego <diegofreeman78@gmail.com>
2025-02-20 08:53:59 +0100

    Update Skeleton components to enhance size and layout for better loading representation

--- components/ui/Skeletons.tsx
+++ components/ui/Skeletons.tsx
@@ -8,7 +8,7 @@ import { HStack } from '@chakra-ui/react';
 export const CategorySwitcherSkeleton = () => {
   return (
     <HStack width="150px">
-      <Skeleton height={40} />
+      <Skeleton height={'50px'} width={'50px'} />
       <SkeletonText noOfLines={1} />
     </HStack>
   );
@@ -23,7 +23,7 @@ export const AccountSwitcherSkeleton = () => {
 export const HeaderLeftSkeleton = () => {
   return (
     <HStack width="200px" gap={3}>
-      <Skeleton height={40} />
+      <Skeleton height={40} width={'200px'} />
       <SkeletonCircle size={'10'} />
     </HStack>
   );
